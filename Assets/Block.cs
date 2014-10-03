0e6fa2f1efc02d0c22d3b60d2ac2c5fffd6ba95b
unknown <piechota33@gmail.com>
2014-10-03 16:47:54 +0200

    xzxz

---
+++ Assets/Block.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+    public GameObject block;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKey(KeyCode.D))
+        {
+
+        }
+
+	}
+}
