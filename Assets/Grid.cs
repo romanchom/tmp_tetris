98385654cf50190db3237e298bd56ce731761844
unknown <piechota33@gmail.com>
2014-10-03 17:14:04 +0200

    dsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdfdsadasdasdsdasdfdafdfasdf

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -3,15 +3,30 @@ using System.Collections;
 
 public class Grid : MonoBehaviour {
 
+    public int score;
+
     public static GameObject[,] grid;
+
+    public GameObject[] blocks; 
 
 	// Use this for initialization
 	void Start () {
         grid = new GameObject[10, 20];
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
+
+    public void Spawn()
+    {
+        int blocksCount = blocks.Length;
+        int block = Random.Range(0, blocksCount);
+
+        int x = Random.Range(0, 10);
+        int y = 21;
+
+        GameObject.Instantiate(blocks[block], new Vector2(x, y), blocks[block].transform.rotation);
+    }
 }
