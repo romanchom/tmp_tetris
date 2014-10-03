8e73ae5342247b66c9e171d6d8b65c9e1f54ca80
romanchom <romanchom@gmail.com>
2014-10-03 16:59:10 +0200

    sdgsgsdf

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -2,21 +2,47 @@
 using System.Collections;
 
 public class Block : MonoBehaviour {
-
     public GameObject block;
+	public GameObject[] components;
+
+	int x, y;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 	
         if(Input.GetKey(KeyCode.D))
         {
 
         }
+	}
 
+	void move(int dx, int dy) {
+		bool isOK = true;
+		foreach (GameObject g in components) {
+			int newX = x + dx + (int)g.transform.localPosition.x;
+			int newY = y + dy + (int)g.transform.localPosition.y;
+			try {
+				if (Grid.grid[newX, newY] != null) {
+					isOK = false;
+					break;
+				}
+			}
+			catch {
+				isOK = false;
+				break;
+			}
+		}
+
+		if (isOK) {
+			// do move
+		}
+		else if (dy != 0) {
+			// zakończ
+		}
 	}
 }
