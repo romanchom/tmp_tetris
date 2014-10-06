8c98bb85b95cbe38b4bd885b862b7a6aaa7b489c
unknown <piechota33@gmail.com>
2014-10-06 23:10:44 +0200

    some shit

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -3,37 +3,70 @@ using System.Collections;
 
 public class Block : MonoBehaviour {
 	public GameObject[] components;
+    public int width = 0;
+    public int height = 0;
+    public Grid grid;
 
 	int x, y;
 
-	void move(int dx, int dy) {
-		bool isOK = true;
-		foreach (GameObject g in components) {
-			int newX = x + dx + (int)g.transform.localPosition.x;
-			int newY = y + dy + (int)g.transform.localPosition.y;
-			try {
-				if (Grid.instance.grid[newX, newY] != null) {
-					isOK = false;
-					break;
-				}
-			}
-			catch {
-				isOK = false;
-				break;
-			}
-		}
+    void Start()
+    {
+        x = (int)transform.position.x;
+        y = (int)transform.position.y;
 
-		if (isOK) {
-			
-		}
-		else if (dy != 0) {
-			foreach (GameObject g in components) {
-				int newX = x + dx + (int)g.transform.localPosition.x;
-				int newY = y + dy + (int)g.transform.localPosition.y;
+        foreach (GameObject comp in components)
+        {
+            if (comp.transform.localPosition.y > height)
+                height = (int)comp.transform.localPosition.y;
+        }
+        height++;
+    }
 
-				Grid.instance.grid[newX, newY] = g;
-			}
-			Destroy(gameObject);
-		}
+    public int GetWidth()
+    {
+        foreach (GameObject comp in components)
+        {
+            if (comp.transform.localPosition.x > width)
+                width = (int)comp.transform.localPosition.x;
+        }
+        width++;
+
+        return width;
+    }
+
+	public void move(int dx, int dy) {
+
+        bool end = false;
+
+        foreach (GameObject comp in components)
+            grid.grid[(int)comp.transform.position.x, (int)comp.transform.position.y] = null;
+
+		foreach(GameObject comp in components)
+        {
+            int x_comp = (int)comp.transform.position.x + dx;
+            int y_comp = (int)comp.transform.position.y + dy;
+
+            if(x_comp < 0 || x_comp >= grid.grid.GetLength(0) ||
+                y_comp < 0 || y_comp >= grid.grid.GetLength(1) ||
+                grid.grid[x_comp, y_comp] != null)
+            {
+                if (y_comp < 0 || grid.grid[(int)comp.transform.position.x, y_comp] != null)
+                    end = true;
+
+                dx = 0;
+                dy = 0;
+
+             
+                break;
+            }
+        }
+
+        transform.position = transform.position + new Vector3(dx, dy, 0);
+
+        foreach (GameObject comp in components)
+            grid.grid[(int)comp.transform.position.x, (int)comp.transform.position.y] = comp;
+
+        if (end)
+            grid.Spawn();
 	}
 }
--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -2,49 +2,100 @@
 using System.Collections;
 
 public class Grid : MonoBehaviour {
-	public static Grid instance;
 
     public int score;
 
     public GameObject[,] grid;
-    public GameObject[] blocks; 
+    public GameObject[] blocks;
 
+    private Block current;
+
+    public float tileUpdate;
+    private float updateTime;
 	// Use this for initialization
 	void Start () {
         grid = new GameObject[10, 25];
-
+        Spawn();
 	}
 	
 	// Update is called once per frame
+    void Update()
+    {
+        if (current && updateTime <= 0)
+        {
+            current.move(0, -1);
+            updateTime = tileUpdate;
+        }
+        else
+            updateTime -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.D))
+            current.move(1, 0);
+
+        if (Input.GetKeyDown(KeyCode.A))
+            current.move(-1, 0);
+    }
 
     public void Spawn()
     {
         int blocksCount = blocks.Length;
         int block = Random.Range(0, blocksCount);
 
-        int x = Random.Range(0, 10);
+        current = (GameObject.Instantiate(blocks[block], new Vector2(0, 0), blocks[block].transform.rotation) as GameObject).GetComponent<Block>();
+        int x = Random.Range(0, 10 - current.GetWidth());
         int y = 21;
 
-        GameObject.Instantiate(blocks[block], new Vector2(x, y), blocks[block].transform.rotation);
+        current.transform.position = new Vector2(x, y);
+        current.grid = this;
+
+        foreach(GameObject comp in current.components)
+        {
+            grid[(int)comp.transform.position.x, (int)comp.transform.position.y] = comp;
+        }
+
+        checkFullLines();
     }
 
 	public void checkFullLines() {
 		for (int i = 0; i < 20; i++) {
 			bool full = true;
 			for (int j = 0; j < 10; ++j) {
-				full &= grid[j, i] != null;
-				if (!full) break;
+				if(grid[j, i] == null)
+                {
+                    full = false;
+                    break;
+                }
 			}
-
-			if (full) removeLine(i);
+            if (full)
+            {
+                removeLine(i);
+                i--;
+            }
 		}
 	}
 
 	void removeLine(int line) {
-		for (int i = line; i < 20; i++) {
-			for (int j = 0; j < 10; j++) {
-				grid[j, i] = grid[j, i + 1];
-			}
-		}
+
+        score += 10;
+
+        for (int x = 0; x < 10; x++)
+        {
+            GameObject obj = grid[x, line];
+            grid[x, line] = null;
+            Destroy(obj);
+        }
+
+		for(int y = line+1; y < 20; y++)
+        {
+            for(int x = 0; x < 10; x++)
+            {
+                GameObject obj = grid[x, y];
+                if (!obj)
+                    continue;
+                grid[x, y - 1] = obj;
+                grid[x, y] = null;
+                obj.transform.position = obj.transform.position + new Vector3(0, -1, 0);
+            }
+        }
 	}
 }
