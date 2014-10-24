a04ef61ae5da27c819f6fb79acd4b222b22d829e
romanchom <romanchom@gmail.com>
2014-10-24 14:09:07 +0200

    KLAWA ZABAWA

--- Assets/scripts/Music.cs
+++ Assets/scripts/Music.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class Music : MonoBehaviour {
 	public AudioSource intro;
 	public AudioSource loop;
+	static bool adsda;
 
 	// Use this for initialization
 	void Start () {
+		if (adsda) {
+			Destroy(gameObject);
+			return;
+		}
+		adsda = true;
 		DontDestroyOnLoad(gameObject);
 		intro.Play();
 		loop.PlayScheduled(AudioSettings.dspTime + intro.clip.length);
